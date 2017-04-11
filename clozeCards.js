98b7cef0cd80e82b85f1f1d5eb10223be8390cb3
Antonio Caos Jr <antoniocaosjr.@Antonios-MacBook-Pro.local>
2017-04-11 19:19:10 -0400

    second commit

--- clozeCards.js
+++ clozeCards.js
@@ -1,11 +1,12 @@
 // create constructor function for basic flashcards in main.js
 // create constructor function for clozecards
-var ClozeCard = function(text, cloze){
+var ClozeCard = function(partial, cloze, full){
 	this.Clozecardarray = [];
-	this.text = text;
+	this.partial = partial;
 	this.cloze = cloze;
-	this.addOne = function(p, c) {
-    	this.Clozecardarray.push(new ClozeCard(p, c));
+	this.full = full;
+	this.addOne = function(p, c, f) {
+    	this.Clozecardarray.push(new ClozeCard(p, c, f));
   };
 }
 
--- main.js
+++ main.js
@@ -2,106 +2,145 @@ var inquirer = require("inquirer");
 var ClozeCard = require("./clozeCards.js");
 var BasicCard = require("./basicCard.js");
 var count = 0;
+var correct = 0;
+var incorrect = 0;
 var type = process.argv[2];
 
-// create cards
- 
- var newBasicCard = new BasicCard();
- newBasicCard.addCard("Who was the first US President?", "George Washington");
- newBasicCard.addCard("What political party was a Bill Clinton affiliated with?", "Democratic");
- newBasicCard.addCard("How many original colonies were there in the United States?","13");
- newBasicCard.addCard("Was President Nixon impeached from office?", "Yes");
+var newBasicCard = new BasicCard();
+newBasicCard.addCard("Who was the first US President?", "George Washington");
+newBasicCard.addCard("What political party was a Bill Clinton affiliated with?", "Democratic");
+newBasicCard.addCard("How many original colonies were there in the United States?","13");
+newBasicCard.addCard("Was President Nixon impeached from office?", "Yes");
 
 
+var newClozecard = new ClozeCard();
+newClozecard.addOne("... was our first President", "George Washington", "George Washington was our first President");
+newClozecard.addOne("... was a Democratic President who got impeached", "Bill Clinton", "Bill Clinton was a Democratic President who got impeached");
+newClozecard.addOne("There was originally ... colonies", "13", "There was originally 13 colonies");
+newClozecard.addOne("... was a Republican President who was impeached from office", "Richard Nixon", "President Nixon was a Republican President who was impeached from office");
 
-
- var newClozecard = new ClozeCard("[hidden] was our first President", "george washington");
- newClozecard.addOne("[hidden] was a Democrat", "bill clinton");
- newClozecard.addOne("There was originally [hidden] colonies","13");
- newClozecard.addOne("[hidden] was impeached from office", "president nixon");
-// create a for loop that loops through the flashcards and calls the function repeatedly
-
-
-
-// var askClozeQuestion = function(){
-
-// 	if (count < 5) {
-// 		 inquirer.prompt([
-// 	      {
-// 	        name: "name",
-// 	        message: newClozecard.Clozecardarray[count].front,
-// 	      }, 
-// 	    ]).then(function(answers) {
-// 	        if (answers.name === newClozecard.Clozecardarray[count].back) {
-// 	        	console.log("correct!");
-	        	
-// 	        }else{
-// 	        	console.log("Incorrect!");
-// 	        }
-// 	        count++;
-// 	        console.log(count);
-// 	        console.log()
-// 	    	askClozeQuestion();
-
-// 	 	 });
-//     }
-// }
-// askClozeQuestion();
-
-
-var askQuestion = function(){
-
-	if (count < 5) {
-		 inquirer.prompt([
-	      {
-	        name: "name",
-	        message: newBasicCard.BasicCardArray[count].front,
-	      }, 
-	    ]).then(function(answers) {
-	        if (answers.name === newBasicCard.BasicCardArray[count].back) {
-	        	console.log("correct!");
-	        	
-	        }else{
-	        	console.log("Incorrect!");
-	        }
-	        count++;
-	        console.log(count);
-	        console.log()
-	    	askQuestion();
-
-	 	 });
-    }
+var startGame = function(){
+		count = 0;
+		correct = 0;
+		incorrect = 0;
 }
+
+var askClozeQuestion = function(){
+	var theClozeQuestion = function(){
+		if (count < newClozecard.Clozecardarray.length) {
+			 inquirer.prompt([
+		      {
+		        name: "name",
+		        message: newClozecard.Clozecardarray[count].partial,
+		      }, 
+		    ]).then(function(answers) {
+			        if (answers.name === newClozecard.Clozecardarray[count].cloze) {
+			        	console.log("correct!");
+			        	console.log(newClozecard.Clozecardarray[count].full)
+			        	correct++;
+			        	
+			        }
+			        else{
+			        	console.log("Incorrect!");
+			        	incorrect++;
+			        }
+		        count++;
+		    	askClozeQuestion();
+		 	 });
+	    }
+	    else{
+	    	console.log("Game Over!");
+	    	if (correct > incorrect) {
+	    		console.log("You Won!");
+	    	}
+	    	else{
+		    	console.log("You Lost!");
+		    }
+// ask to play again
+		    inquirer.prompt([
+		    {
+		        name: "name",
+		        message: "Play Again?",
+		    }, 
+		    ]).then(function(answers) {
+		    	if (answers.name === "Yes" || "yes") {
+		    		count = 0;
+		    		correct = 0;
+		    		incorrect = 0;
+		    		askClozeQuestion();
+
+		    	}
+	    	   });
+		}
+    }
+    theClozeQuestion();
+}
+
+
+var askBasicQuestion = function(){
+	var askQuestion = function(){
+		if (count < newBasicCard.BasicCardArray.length) {
+			 inquirer.prompt([
+		      {
+		        name: "name",
+		        message: newBasicCard.BasicCardArray[count].front,
+		      }, 
+		    ]).then(function(answers) {
+		        if (answers.name === newBasicCard.BasicCardArray[count].back) {
+		        	console.log("correct!");
+		        	correct++;
+		        	count++;
+		    		askQuestion();
+		        	
+		        }else{
+		        	console.log("Incorrect!");
+					incorrect++;
+					count++;
+		    		askQuestion();	
+		        }
+		 	 });
+	    }
+	    else{
+	    	console.log("Game Over!");
+	    	if (correct > incorrect) {
+	    		console.log("You Won!");
+	    	}
+	    	else{
+		    	console.log("You Lost!");
+		    }
+// ask to play again
+		    inquirer.prompt([
+		    {
+		        name: "name",
+		        message: "Play Again?",
+		    }, 
+		    ]).then(function(answers) {
+		    	if (answers.name === "Yes" || "yes") {
+		    		count = 0;
+		    		correct = 0;
+		    		incorrect = 0;
+		    		askBasicQuestion();
+
+		    	}
+		    	else{
+					startGame();
+		    	}	
+	    	   });
+		}
+	
+	}
+askQuestion();
+}
+
+
 switch(type){
 	case "Basic-Card": 
-	askQuestion();
+	askBasicQuestion();
     break;
     case "Partial-Card": 
 	askClozeQuestion();
     break;
 }
-
-
-// create a user generated conditional that stops the for loop
-
-// store those inputs to two separate arrays one for cloze flashcards one for Basic flashcards 
-
-// output the clozecards to an external clozecards text file
-
-// output the basic cards to an external basic cards text file
-
-// require the external log files 
-
-// create a new js file that is the flashcard game
-
-// make the user input call a flashcard at random from either the clozecards file or the array
-
-// they will use process argv [2] as the type of flashcard to pull
-
-
-
-
-
 
 
 
